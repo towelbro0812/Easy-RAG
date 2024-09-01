e7dbf53378573379db3efc1a8847068d36eb276f
towelbro0812 <kevinjoy7271571@gmail.com>
2024-09-01 11:04:44 +0800

    First commit

---
+++ query_data.py
@@ -0,0 +1,55 @@
+import argparse
+from langchain_community.llms.ollama import Ollama
+
+from get_embedding_function import get_embedding_function
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
+
+
+def query_rag(query_text: str):
+    # Prepare the DB.
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search(query_text, k=5)
+    print(results[0].metadata.get("id", None))
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    # print(prompt)
+
+    # model = Ollama(model="mistral")
+    model = Ollama(model="llama3.1")
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("id", None) for doc in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+    return response_text
+
+
+if __name__ == "__main__":
+    main()
